be775c2e747b7b44d1b98b9645b98be1c31b88f0
Thiago Costa <tcostacoelho@gmail.com>
2017-07-14 11:54:03 -0300

    Primeiro commit statics

---
+++ runze/static/runze/bootstrap/js/form-contato.js
@@ -0,0 +1,20 @@
+$('.form-contato').on('submit', function(e){
+	e.preventDefault();
+	var valores = $(this).serialize();
+	var loading = $('.loading');
+	$('#myModal').modal('show');
+	$.post(this.action, valores, function(data){
+		$('#myModal').hide();	
+		$('.modal-backdrop').hide();
+		$('body').removeClass('modal-open');	
+		$('#div-contato').html(data);
+	});	
+});
+
+function myMap() {
+	var myCenter = new google.maps.LatLng(41.878114, -87.629798);
+	var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
+	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	var marker = new google.maps.Marker({position:myCenter});
+	marker.setMap(map);
+}
---
+++ runze/static/runze/bootstrap/js/script.js
@@ -0,0 +1,30 @@
+$('.menu-item').on('click', function(e){
+	e.preventDefault();
+	topoHt = $(".navbar").height()+16	;
+	var id = $(this).attr('id');
+	var sec = $('#conteudo').find('section.'+id)
+	$("html, body").animate({scrollTop: sec.offset().top-topoHt}, 200);
+});
+$('.form-contato').on('submit', function(e){
+	e.preventDefault();
+	var valores = $(this).serialize();
+	$('#myModal').modal('show');
+	$.post(this.action, valores, function(data){
+		$('#myModal').hide();	
+		$('.modal-backdrop').hide();
+		$('body').removeClass('modal-open');	
+		$('#div-contato').html(data);
+	}).fail(function(){
+		$('#myModal').hide();	
+		$('.modal-backdrop').hide();
+		$('body').removeClass('modal-open');	
+	});	
+});
+
+function myMap() {
+	var myCenter = new google.maps.LatLng(-5.527286, -47.492012);
+	var mapProp = {center:myCenter, zoom:16, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
+	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	var marker = new google.maps.Marker({position:myCenter});
+	marker.setMap(map);
+}
