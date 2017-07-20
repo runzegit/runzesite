f850521fd64a44d0f28796b33aa4ffb2863cf9d3
Thiago Costa <tcostacoelho@gmail.com>
2017-07-20 09:54:10 -0300

    Erro de identacao 2

--- runze/views.py
+++ runze/views.py
@@ -16,18 +16,18 @@ def home(request):
 	form = ContatoForm()
 	for menu in menus:
 		conteudos = Conteudo.objects.filter(menu=menu)
-		menu.conteudos = conteudos
+	menu.conteudos = conteudos
 	return render(request, 'runze/home.html', {'empresa':empresa, 'menus': menus, 'carousel': carousel, 'form': form})
 
 def contato(request):
 	if request.method == "POST":
-	 	form = ContatoForm(request.POST)
+		form = ContatoForm(request.POST)
 		if form.is_valid():
 			contato = form.save(commit=False)
 			contato.save()
 			empresa = Empresa.objects.get(pk=1)
 			texto = 'Nome: '+contato.nome+'\n\r'+'Telefone: '+contato.telefone+'\n\r'+'E-mail: '+contato.email+'\n\r'+'Data: '+str(contato.data)+'\n\r\n\r'+'Assunto: '+contato.assunto+'\n\r\n\r'+'Mensagem: '+contato.texto
 			time.sleep(5)
-			send_mail('Site: '+contato.nome+' - '+contato.assunto, texto, 'runzegit@gmail.com', ['runzegit@gmail.com'], fail_silently=False, )
+			#send_mail('Site: '+contato.nome+' - '+contato.assunto, texto, 'runzegit@gmail.com', ['runzegit@gmail.com'], fail_silently=False, )
 			return render(request, 'runze/form-contato.html', {'form': ContatoForm(), 'resposta': 'Mensagem enviada com sucesso! Obrigado!'})
-	return render(request, 'runze/form-contato.html', {'form': ContatoForm(), 'resposta': 'Ocorrou algum problema ao enviar mensagem, tente novamente mais tarde. Obrigado'})
+		return render(request, 'runze/form-contato.html', {'form': ContatoForm(), 'resposta': 'Ocorrou algum problema ao enviar mensagem, tente novamente mais tarde. Obrigado'})
