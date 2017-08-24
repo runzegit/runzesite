35bdd0f953ab165a4a45eb26a1b7168ae865d0ff
Thiago Costa <tcostacoelho@gmail.com>
2017-08-24 10:48:13 -0300

    Urls

--- runzesite/urls.py
+++ runzesite/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 	url(r'^.*/$', include('producao.urls')),
-
+	url(r'^', include('producao.urls')),
 	#url(r'^admin/', admin.site.urls),
 ]
 
